035e666e0a4f97376b130d56baf5e6fe1adc48d2
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 15:25:44 +0100

    Fixing a bug

---
+++ src/tests/unit/userModel.test.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const userModel = require('../../models/userModel');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongo = await MongoMemoryServer.create();
+
+// @Todo:
+// Save a new user
+// check for incorrect details : name, email, password
+// check for required field
+
+describe('Save user', () => {
+    beforeAll(async () => {
+        const uri = await mongod.getUri()
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+    });
+
+
+    afterAll(async () => {
+        await mongoose.disconnect();
+        await mongod.stop();
+    });
+
+    test('Should save a new user to the database', async () => {
+        const userData = {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'johndoe@example.com',
+            password: 'password123'
+        };
+        const newUser = new userModel(userData);
+        await newUser.save();
+
+        const savedUser = await userModel.findOne({email: 'johndoe@example.com'})
+        expect(savedUser).toMatchObject(userData);
+    });
+});
