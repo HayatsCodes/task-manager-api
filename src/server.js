2b6c58dec8d6e394993888e83121e5251fda3553
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 10:55:38 +0100

    Refactoring the code

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const app = require('./app');
+const mongoConnect = require('./config/db')
 
 const server = http.createServer(app);
 
 
 
-function startServer() {
+async function startServer() {
+
     server.listen(4040, () => {
         console.log('Listening on port 4040');
     });
