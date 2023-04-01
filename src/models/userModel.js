96a98cb7740238f4cc79d233ef0b966672d6fdbf
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 15:47:44 +0100

    Updating the documentation

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { validate } = require('./taskModel');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -14,8 +15,8 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
-        
+        unique: true,
+        validate: []
     },
     password: {
         type: String,
