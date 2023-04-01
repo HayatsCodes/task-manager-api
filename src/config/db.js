64a0de2d952c3ab3c22dc836b0646ab8649eed16
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 10:11:01 +0100

    Refactoring the code

--- src/config/db.js
+++ src/config/db.js
@@ -3,5 +3,10 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 mongoose.connection.once('open', () => {
-    console.log('MongoDB connected successfully')
-})
+    console.log('MongoDB connected successfully');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+});
+
