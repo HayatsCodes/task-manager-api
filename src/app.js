345ef7fcf2cb9454e263de556480b61c928359ce
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 10:37:49 +0100

    Updating the code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('combined'));
 
 module.exports = app;
