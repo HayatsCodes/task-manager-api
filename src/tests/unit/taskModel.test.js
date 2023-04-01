0d6b0db396aeca89a12cda5538592f8fd6db454f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 22:14:16 +0100

    Refactoring the code

--- src/tests/unit/taskModel.test.js
+++ src/tests/unit/taskModel.test.js
@@ -38,7 +38,7 @@ describe('Task Model', () => {
             };
 
             const savedTask = await taskModel.findOne({ owner: user._id });
-            expect()
+            expect(saved)
         });
     })
 });
