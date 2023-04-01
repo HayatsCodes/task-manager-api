b6d18cc0d74229fc9899ebf69655792c2f625576
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 15:49:46 +0100

    Improving performance

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        validate: []
+        validate: [validator.is]
     },
     password: {
         type: String,
