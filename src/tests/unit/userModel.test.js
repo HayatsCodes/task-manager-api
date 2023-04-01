f574fe9e0a7d1c9e0996a7b47f05e5c210dc422f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 15:40:44 +0100

    Fixing a bug

--- src/tests/unit/userModel.test.js
+++ src/tests/unit/userModel.test.js
@@ -3,34 +3,34 @@ const userModel = require('../../models/userModel');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
 describe('userModel', () => {
-  let mongo;
+    let mongo;
 
-  beforeAll(async () => {
-    mongo = await MongoMemoryServer.create();
-    const uri = mongo.getUri();
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    beforeAll(async () => {
+        mongo = await MongoMemoryServer.create();
+        const uri = mongo.getUri();
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
     });
-  });
 
-  afterAll(async () => {
-    await mongoose.disconnect();
-    await mongo.stop();
-  });
-
-  describe('Save user', () => {
-    test('Should save a new user to the database', async () => {
-      const userData = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-      };
-      await userModel.create(userData);
-
-      const savedUser = await userModel.findOne({ email: 'johndoe@example.com' });
-      expect(savedUser).toMatchObject(userData);
+    afterAll(async () => {
+        await mongoose.disconnect();
+        await mongo.stop();
     });
-  });
+
+    describe('Save user', () => {
+        test('Should save a new user to the database', async () => {
+            const userData = {
+                firstName: 'John',
+                lastName: 'Doe',
+                email: 'johndoe@example.com',
+                password: 'password123',
+            };
+            await userModel.create(userData);
+
+            const savedUser = await userModel.findOne({ email: 'johndoe@example.com' });
+            expect(savedUser).toMatchObject(userData);
+        });
+    });
 });
