4555cd20fd624b8950a0c3e97cc0fdc949e332b4
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 15:36:55 +0100

    Updating the code

--- src/tests/unit/userModel.test.js
+++ src/tests/unit/userModel.test.js
@@ -2,39 +2,54 @@ const mongoose = require('mongoose');
 const userModel = require('../../models/userModel');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
-// @Todo:
-// Save a new user
-// check for incorrect details : name, email, password
-// check for required field
+describe('userModel', () => {
+  let mongo;
 
-describe('Save user', async () => {
-
-    const mongo = await MongoMemoryServer.create();
-    beforeAll(async () => {
-        const uri = mongo.getUri()
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+  beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const uri = mongo.getUri();
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+  });
 
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongo.stop();
+  });
 
-    afterAll(async () => {
-        await mongoose.disconnect();
-        await mongo.stop();
-    });
-
+  describe('Save user', () => {
     test('Should save a new user to the database', async () => {
-        const userData = {
-            firstName: 'John',
-            lastName: 'Doe',
-            email: 'johndoe@example.com',
-            password: 'password123'
-        };
-        const newUser = new userModel(userData);
-        await newUser.save();
+      const userData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+        password: 'password123',
+      };
+      await userModel.create(userData);
 
-        const savedUser = await userModel.findOne({email: 'johndoe@example.com'})
-        expect(savedUser).toMatchObject(userData);
+      const savedUser = await userModel.findOne({ email: 'johndoe@example.com' });
+      expect(savedUser).toMatchObject(userData);
     });
+
+    test('Should not save a user with incorrect details', async () => {
+      const userData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'invalid-email',
+        password: 'password123',
+      };
+      await expect(userModel.create(userData)).rejects.toThrow();
+    });
+
+    test('Should not save a user with missing required fields', async () => {
+      const userData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        password: 'password123',
+      };
+      await expect(userModel.create(userData)).rejects.toThrow();
+    });
+  });
 });
