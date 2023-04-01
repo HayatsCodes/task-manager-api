221b12deb0ac8539e3033b9887e8fa31eab338c3
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 21:54:37 +0100

    Updating the code

--- src/tests/unit/taskModel.test.js
+++ src/tests/unit/taskModel.test.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const taskModel = require('../../models/taskModel');
 
-
+describe('Task Model', () => {
+    beforeAll( ())
+});
 
 // @Todo:
 // saves a new task
