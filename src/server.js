7e02123aca2e31f39f78a7863f0bedb985c872e2
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 10:56:39 +0100

    Improving performance

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 
 async function startServer() {
-
+    await mongoConnect();
     server.listen(4040, () => {
         console.log('Listening on port 4040');
     });
