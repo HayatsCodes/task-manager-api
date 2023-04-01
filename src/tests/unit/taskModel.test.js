d39887f4e04bc65fe1b9b5e0de898907d2c79d59
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 22:42:13 +0100

    Fixing a bug

--- src/tests/unit/taskModel.test.js
+++ src/tests/unit/taskModel.test.js
@@ -66,7 +66,7 @@ describe('Task Model', () => {
     });
 
     describe('reject task with missing required fields', () => {
-        
+        test('should reject missing description field', ())
     })
 });
 
