7be88065631a2098c57f2a7db58f6472c8cccbaa
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 10:51:17 +0100

    Updating the documentation

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
+
+
 function startServer() {
     server.listen(4040, () => {
         console.log('Listening on port 4040');
