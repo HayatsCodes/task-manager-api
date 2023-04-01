0ea7abf670803d7307e16561a5f552ad569aeb7c
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-01 10:14:06 +0100

    Updating the code

--- src/config/db.js
+++ src/config/db.js
@@ -12,6 +12,9 @@ mongoose.connection.on('error', (err) => {
 
 async function mongoConnect() {
     await mongoose.connect(process.env.MONGO_URI, {
-        
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     });
 }
+
+module.exports = mongoConnect;
