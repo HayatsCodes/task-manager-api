946d9b5f57c458dc4028d516cfcb738371810d54
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 00:35:02 +0100

    Updating the code

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,5 +4,7 @@ const userModel = require('../models/userModel');
 async function authMiddleware (req, res, next) {
     try {
         
+    } catch (error) {
+        return res.json({ error });
     }
 }
