3ef1a2dac65186a0c4f63cdf9750d26777ae4689
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 00:19:42 +0100

    Updating the documentation

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -3,7 +3,8 @@ const taskModel = require('../models/taskModel');
 async function addTask(req, res) {
     try {
         const task = new taskModel({
-            
+            ...req.body,
+            owner: req.user._id
         })
     } catch (error) {
         return res.json({ error });
