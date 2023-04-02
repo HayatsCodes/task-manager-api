1d6908b977d4bd637ec3986d0bc74939f0134af7
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-02 15:29:38 +0100

    Refactoring the code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,7 +25,7 @@ async function registerUser(req, res) {
 
         const token = await jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
             expiresIn: process.env.JWT_EXPIRE
-        });c
+        });
         return res.status(201).cookie({'token': token}).json({success: true, message: 'User registered successfully', data: user})
     } catch (error) {
         return res.json({ error: error });
