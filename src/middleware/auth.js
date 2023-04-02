8f82f40920ce246bbd0490daa7a24df4dd144b13
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 00:38:08 +0100

    Improving performance

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,9 @@ const userModel = require('../models/userModel');
 
 async function authMiddleware (req, res, next) {
     try {
-        
+        const { token } = req.cookies;
+
+        if(!token)
     } catch (error) {
         return res.json({ error });
     }
