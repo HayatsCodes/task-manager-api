f816a3591474c6ed7e5964f785561b557e0a4612
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-02 15:27:36 +0100

    Updating the code

--- src/tests/unit/userModel.test.js
+++ src/tests/unit/userModel.test.js
@@ -64,7 +64,7 @@ describe('userModel', () => {
               await expect(userModel.create(userData2)).rejects.toThrow();
         });
 
-        test('Should reject passwords less than 8 and greater than 15 characters', async () => {
+        test('Should reject passwords less than 8 characters', async () => {
             const userData = {
                 firstName: 'Jane',
                 lastName: 'Doe',
