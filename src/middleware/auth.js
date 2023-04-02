7f8d087ceef6089051f1986206eb1544eb4d293b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 00:28:45 +0100

    Updating the code

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,5 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/userModel');
 
 async function authMiddleware (req, res, next) {
     
