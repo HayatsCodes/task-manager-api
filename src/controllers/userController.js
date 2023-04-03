069de6ba5a52acbc0104d18783aada72681cf3eb
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 16:37:27 +0100

    Updating the code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -60,6 +60,7 @@ async function loginUser(req, res) {
     
         const token = await jwt.sign({id: userId}, process.env.SECRET_KEY, {
             expiresIn: process.env.JWT_EXPIRE,
+            
         });
 
         user.token = token;
