c46048f80960167ebb19ffc8a364182e014aa395
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 22:41:37 +0100

    Updating the code

--- src/tests/integration/user.test.js
+++ src/tests/integration/user.test.js
@@ -1,4 +1,6 @@
 // @Todo:
 //  1. Test the auth/register endpoint:
 //      a. Should register a user succesfully
-//   
+//  2. Test the auth/login endpoint:
+//      b. should login a registered user succesfully
+//      c. sould reject an unregistered user 
