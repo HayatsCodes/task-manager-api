b2076509e1b82f2ea531eb240afa074c6b9b436c
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 23:15:56 +0100

    Updating the documentation

--- src/tests/integration/user.test.js
+++ src/tests/integration/user.test.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../../app');
 
+describe('User endpoints', () => {
 
 
-describe('POST auth/register', () => {
+    describe('POST auth/register', () => {
 
-    it('Should register a user succesfully', () => {
-
+        it('Should register a user succesfully', () => {
+    
+        });
     });
-});
+})
+
+
 // @Todo:
 //  1. Test the auth/register endpoint:
 //      a. Should register a user succesfully
