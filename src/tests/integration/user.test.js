936844f462db3b8c6fe28a758cd0efbcedeb172e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 22:52:47 +0100

    Adding a new feature

--- src/tests/integration/user.test.js
+++ src/tests/integration/user.test.js
@@ -1,6 +1,7 @@
 const supertest = require('supertest');
 const app = require('../../app');
 
+describe('POST auth/register', ())
 // @Todo:
 //  1. Test the auth/register endpoint:
 //      a. Should register a user succesfully
