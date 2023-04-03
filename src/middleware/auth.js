b5f9820568bdd548ccf8948129e8b3e656620a9f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 17:34:06 +0100

    Fixing a bug

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -13,7 +13,6 @@ async function authMiddleware (req, res, next) {
         const decoded = await jwt.verify(token, process.env.SECRET_KEY);
         console.log(token);
         const user = await userModel.findOne({ _id: decoded.id, 'token': token});
-
         
         if(!user) {
             return res.status(400).json({ message: 'Authentication failed' });
