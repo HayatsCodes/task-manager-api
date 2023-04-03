2b8ac7f9599fbd6ea94dcdc92f7b2197696f4192
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 10:50:50 +0100

    Improving performance

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -53,6 +53,8 @@ async function loginUser(req, res) {
         }
     
         const userId = verifyUser(email, password);
+
+        
     
         const token = await jwt.sign({id: userId}, process.env.SECRET_KEY, {
             expiresIn: process.env.JWT_EXPIRE,
