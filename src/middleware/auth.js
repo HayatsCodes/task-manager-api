e5c3251c62b6d27ea0ede51ea2a1b15fcb8a42de
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-03 11:12:28 +0100

    Adding a new feature

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,8 @@ const userModel = require('../models/userModel');
 async function authMiddleware (req, res, next) {
     try {
         const { token } = req.cookies;
-        
+
+        console.log(token);
 
         if(!token) {
         return res.status(400).json({ message: 'Authentication failed' });
