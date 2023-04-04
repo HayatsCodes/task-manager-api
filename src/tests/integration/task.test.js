6e18801ee9dc2e2c306187ca4de5687943e3adeb
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-04 15:34:11 +0100

    Updating the documentation

--- src/tests/integration/task.test.js
+++ src/tests/integration/task.test.js
@@ -124,8 +124,6 @@ describe('Task endpoints', () => {
             .expect('Content-Type', /json/)
             .expect(200);
 
-            // expect(res.body.description).toBe('First task');
-            expect(res.body.completed).toBe(true);
             expect(res.body.owner).toBe(owner);
 
         });
