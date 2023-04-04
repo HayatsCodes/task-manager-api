2f941196157d67c43800b0cf20e915b65d324532
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-04 15:21:36 +0100

    Improving performance

--- src/tests/integration/task.test.js
+++ src/tests/integration/task.test.js
@@ -59,6 +59,7 @@ describe('Task endpoints', () => {
             expect(res.body.task.owner).toBe(owner);
             id = res.body._id;
 
+            console.log(id)
         });
 
         it('Should not add a task for users with invalid token', async () => {
