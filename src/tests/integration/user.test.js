6629f93e18302bc68fc12b9c2b263d007834a342
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-04 10:01:14 +0100

    Updating the documentation

--- src/tests/integration/user.test.js
+++ src/tests/integration/user.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const { MongoMemoryServer } = require('mongodb-memory-server');
+require('dotenv').config();
 const app = require('../../app');
 
 describe('User endpoints', () => {
