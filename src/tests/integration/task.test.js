99e8b266bad62d11473ec5b32d9e54546bf34b3b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-04 12:08:58 +0100

    Adding a new feature

--- src/tests/integration/task.test.js
+++ src/tests/integration/task.test.js
@@ -22,7 +22,11 @@ describe('Task endpoints', () => {
         mongo.stop();
     });
 
-    
+    describe('POST /tasks', () => {
+        it('Should add a test succesfully for logged in user', () => {
+            
+        });
+    })
 
 });
 
