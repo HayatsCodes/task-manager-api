c7062dc93678aaddc1e0710dc962ef23c90a627e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-06 22:11:53 +0100

    Fixing a bug

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -41,7 +41,7 @@ async function getTasks(req, res) {
         if (!tasks) {
             return res.json({ error: 'No task found' });
         }
-        redisClient.set(owner, JSON.stringify({ 'getTasks': tasks }));
+        redisClient.set(owner, JSON.stringify(tasks));
         return res.json({ 'tasks': tasks });
     } catch (error) {
         return res.status(400).json({ error });
