21ece98ece64569c7de82cee2f7a658f00525d59
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-06 17:38:50 +0100

    Adding a new feature

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -36,7 +36,10 @@ async function addTask(req, res) {
 async function getTasks(req, res) {
     try {
         const owner = req.user._id;
-        await redis.get(owner)
+        const cachedOwner = await redis.get(owner);
+        if(cachedOwner) {
+            
+        }
         const tasks = await taskModel.find({'owner': owner});
         if (!tasks) {
             return res.json({ error: 'No task found'});
