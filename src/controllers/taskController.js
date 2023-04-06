b151d396f3e2af47bb5d73b84c9477a2dc33be1d
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-06 17:46:31 +0100

    Refactoring the code

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,6 +1,5 @@
 const Redis = require('ioredis');
 const taskModel = require('../models/taskModel');
-const { json } = require('express');
 
 // Owner should be the key to a redis db
 // the value should be an object 
@@ -41,7 +40,7 @@ async function getTasks(req, res) {
             if (!results) {
                 return null;
             }
-            const tasks = JS
+            const tasks = JSON.parse(results)
         });
         if(cachedOwner) {
             
